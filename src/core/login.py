f95b20ea264d1d940732c9f5d08320650127e372
mursalatul <mursalatulislampallob@gmail.com>
2024-04-22 17:17:35 +0600

    breakdown the username and password checking to check them separatly.

--- src/core/login.py
+++ src/core/login.py
@@ -2,7 +2,9 @@ import src.core.validation as scv
 
 class Login:
     # username and password format status
-    username_password_seems_good = False
+    usernameOk = False
+    passowordOk = False
+
     # validation object from src->core->validation to perform all type of checking
     validator = None
 
@@ -12,11 +14,15 @@ class Login:
         # initialize the validator
         self.validator = scv.FormatVerify()
         # check if the username and password format is ok
-        self.checkFormat()
-        if self.username_password_seems_good:
-            # decrypt the password
-            # check username and password in database
-            pass
+        self._checkFormat()
+        # if self.username_password_seems_good:
+        #     # decrypt the password
+        #     # check username and password in database
+        #     pass
     
-    def checkFormat(self):
-        self.username_password_seems_good = self.validator.varifyUsernameFormat(self._username) and self.validator.verifyPasswordFormat(self._password)
+    def _checkFormat(self):
+        self.usernameOk = self.validator.varifyUsernameFormat(self._username)
+        self.passowordOk = self.validator.verifyPasswordFormat(self._password)
+    
+    def isGoodFormated(self):
+        return self.usernameOk and self.passowordOk
