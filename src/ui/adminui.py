bd9150e607cfe21eec9a461c155d2f61734346e7
mursalatul <mursalatulislampallob@gmail.com>
2024-05-13 03:52:33 +0600

    loaded widgets of book query -> search book -> all widgets

--- src/ui/adminui.py
+++ src/ui/adminui.py
@@ -7,6 +7,9 @@ class AdminUI(QtWidgets.QMainWindow):
     it will be called from login page, when
     a admin will login
     """
+    # this variable will hold the adminui on
+    admin_page = None
+    admin_page_object = {}
     def __init__(self) -> None:
         super(AdminUI, self).__init__()
         # load the admin page ui
@@ -19,6 +22,47 @@ class AdminUI(QtWidgets.QMainWindow):
         self._loadTheWidgets()
     
     def _loadTheWidgets(self):
-        self.widget_main_options = self.findChild(
+        #  widget of the admin page
+        self.admin_page_object["widget_main_options"] = self.findChild(
             QtWidgets.QWidget, "widget_main_options"
         )
+
+        # widget of sub option book query
+        self.admin_page_object["widget_sub_options_book_query"] = self.findChild(
+            QtWidgets.QWidget, "widget_sub_options_book_query"
+        )
+
+        # push button of search book
+        self.admin_page_object["pushButton_search_book"] = self.findChild(
+            QtWidgets.QWidget, "pushButton_search_book"
+        )
+
+        # combo box 
+        self.admin_page_object["comboBox_book_search_by"] = self.findChild(
+            QtWidgets.QComboBox, "comboBox_book_search_by"
+        )
+
+        # combo box targetted line edit
+        self.admin_page_object["lineEdit"] = self.findChild(
+            QtWidgets.QLineEdit, "lineEdit_comboBox_targetted"
+        )
+
+        # search by the line edit button
+        self.admin_page_object["pushButton_for_lineEdit_comboBox_targetted"] = self.findChild(
+            QtWidgets.QPushButton, "pushButton_for_lineEdit_comboBox_targetted"
+        )
+
+        # show book information text browser
+        self.admin_page_object["textBrowser_book_information"] = self.findChild(
+            QtWidgets.QTextBrowser, "textBrowser_book_information"
+        )
+
+        # widget operations main screen
+        self.admin_page_object["widget_operations"] = self.findChild(
+            QtWidgets.QWidget, "widget_operations"
+        )
+
+
+
+
+
