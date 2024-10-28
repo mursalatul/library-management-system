693040cdffdc0c8d8011336b212747bc17c7fdbc
mursalatul <mursalatulislampallob@gmail.com>
2024-10-28 17:01:29 +0600

    remove book function added.

--- src/core/admin_opetation_handler/remove_book.py
+++ src/core/admin_opetation_handler/remove_book.py
@@ -19,12 +19,37 @@ class RemoveBook:
         self.remove_book_object["pushButton_remove_book_remove_button"] = self.adminui.findChild(
             QtWidgets.QPushButton, "pushButton_remove_book_remove_button"
         )
-    def validId(self) -> bool:
+    def validId(self):
+        """
+        Work:
+            1. see if the id is a valid id, if no, go to step 4, else step 2
+            2. if id is valid id, check the id presence in the database books.
+                if found go to step 3,
+                else go to step 4
+            3. return true
+            4. return false
+        Returns:
+            object: False if not found, else the data
+
+        """
         id = self.remove_book_object["lineEdit_remove_book_id"].text()
-        return not (len(id) == 0 or not id.isnumeric())
+        if  (len(id) == 0 or not id.isnumeric()):
+            return False
+        db = Database()
+        status = db.getData('books','book_id', id)
+        if status == None:
+            return False
+        else:
+            # removing the book form the table
+            print(db.deleteData('books','book_id',id))
+            return status
 
     def removeButtonAction(self):
-        if self.validId():
-            self.remove_book_object["textBrowser_remove_book_status"].setPlainText("valid id")
-        else:
+        status = self.validId()
+        print(status)
+        if not status:
           self.remove_book_object["textBrowser_remove_book_status"].setPlainText("Invalid id")
+        else:
+          self.remove_book_object["textBrowser_remove_book_status"].setPlainText(
+              f'Book Removed:\n{status[0]}\t{status[1]}\t{status[2]}')
+
